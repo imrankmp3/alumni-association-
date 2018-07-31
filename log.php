086c36aefc3efc2368e6f99faa5ff8a1b8fb4b67
Imran Mohammed <imrankmp3@gmail.com>
2018-07-31 16:01:11 +0530

    Add files via upload

---
+++ log.php
@@ -0,0 +1,28 @@
+<?php
+
+include 'log2.php';
+
+
+$email = $_POST["username"];
+$password = $_POST["password"];
+
+
+
+if($mysqli->query("INSERT INTO login (username, password) VALUES('$email','$password')"))
+    {
+   
+    echo "Data inserted";
+   
+} 
+
+  else
+  {
+  
+    echo "Data not inserted";
+}
+
+
+
+?>
+
+
